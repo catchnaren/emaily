a4746da7cd41b8fcdd85ee3b4a8d516ab6300717
catchnaren <narenckumar@gmail.com>
2022-06-08 12:59:59 +0530

    Final campaign logic with integration

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'Survey Title',
+    name: 'title',
+    noValueError: 'You must provide a survey title',
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    noValueError: 'You must provide a subject line',
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    noValueError: 'You must provide an email body',
+  },
+  {
+    label: 'Recipient List',
+    name: 'recipients',
+    noValueError: 'You must provide the recipient list',
+  },
+];
