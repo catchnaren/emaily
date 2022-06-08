166458388e6202fdda84f886c15093e87f2e77bf
catchnaren <narenckumar@gmail.com>
2022-06-08 13:15:05 +0530

    Update history url path

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,7 +13,7 @@ export const handleToken = (token) => async (dispatch) => {
 
 export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post('/api/surveys', values);
-  history.push('/surveys');
+  history.push('surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
